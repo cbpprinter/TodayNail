f6d36235dd5c9aaae59c613c7706cdedd2005f92
cbpprinter <cbp.printer@gmail.com>
2017-05-25 18:17:51 +0900

    crop intent

--- app/src/main/java/com/tt/todaynail/MainActivity.java
+++ app/src/main/java/com/tt/todaynail/MainActivity.java
@@ -1,23 +1,36 @@
 package com.tt.todaynail;
-
-import android.os.Bundle;
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.GridView;
-import android.widget.TabHost;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.tt.todaynail.tab1.ImageGridAdapter;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TabHost tabHost;
-
-    private int[] mThumbIds = new int[] { R.drawable.view01,
-            R.drawable.view02, R.drawable.view03,
-            R.drawable.view04, R.drawable.view05,
-            R.drawable.view06, R.drawable.view07
-    };
+    ImageView imageView;
+    Toolbar toolbar;
+    File file;
+    Uri uri;
+    Intent CamIntent,GalIntent,CropIntent;
+    final int RequestPermissionCode=1;
+    DisplayMetrics displayMetrics;
+    int width, height;
 
 
     @Override
@@ -25,41 +38,115 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tabHost = (TabHost) findViewById(R.id.tabHost);
-        tabHost.setup();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Crop Image");
+        setSupportActionBar(toolbar);
 
-        TabHost.TabSpec tab1 = tabHost.newTabSpec("tab1");
-        tab1.setContent(R.id.tab1);
-        tab1.setIndicator(getString(R.string.tab1));
-        GridView gridViewImages = (GridView)findViewById(R.id.gridView1);
-        ImageGridAdapter imageGridAdapter = new ImageGridAdapter(this, mThumbIds);
-        gridViewImages.setAdapter(imageGridAdapter);
+        imageView = (ImageView)findViewById(R.id.imageView);
 
-        TabHost.TabSpec tab2 = tabHost.newTabSpec("tab2");
-        tab2.setContent(R.id.tab2);
-        tab2.setIndicator(getString(R.string.tab2));
 
-        TabHost.TabSpec tab3 = tabHost.newTabSpec("tab3");
-        tab3.setContent(R.id.tab3);
-        tab3.setIndicator(getString(R.string.tab3));
+        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
+        if (permissionCheck == PackageManager.PERMISSION_DENIED)
+            RequestRuntimePermission();
+    }
 
-        tabHost.addTab(tab1);
-        tabHost.addTab(tab2);
-        tabHost.addTab(tab3);
+    private void RequestRuntimePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA))
+            Toast.makeText(this,"CAMERA permission allows us to access CAMERA app",Toast.LENGTH_SHORT).show();
+        else
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA}, RequestPermissionCode);
+        }
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-       getMenuInflater().inflate(R.menu.menu, menu);
+
+        getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.btn_camera){
-            return true;
+        if(item.getItemId() == R.id.btn_camera)
+            CameraOpen();
+        else if(item.getItemId() == R.id.btn_gallery)
+            GalleryOpen();
+        return true;
+    }
+
+    private void GalleryOpen() {
+        GalIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(GalIntent,"Select Image from Gallery"),2);
+    }
+
+    private void CameraOpen() {
+        CamIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = new File(Environment.getExternalStorageDirectory(),
+                "file"+String.valueOf(System.currentTimeMillis())+".jpg");
+        uri = Uri.fromFile(file);
+        CamIntent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+        CamIntent.putExtra("return-data",true);
+        startActivityForResult(CamIntent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0 && resultCode == RESULT_OK)
+            CropImage();
+        else if(requestCode == 2)
+        {
+            if(data != null)
+            {
+                uri = data.getData();
+                CropImage();
+            }
         }
-        return super.onOptionsItemSelected(item);
+        else if(requestCode == 1)
+        {
+            if(data != null)
+            {
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = bundle.getParcelable("data");
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private void CropImage() {
+
+        try{
+            CropIntent = new Intent("com.android.camera.action.CROP");
+            CropIntent.setDataAndType(uri,"image/*");
+
+            CropIntent.putExtra("crop","true");
+            CropIntent.putExtra("outputX",180);
+            CropIntent.putExtra("outputY",180);
+            CropIntent.putExtra("aspectX",3);
+            CropIntent.putExtra("aspectY",4);
+            CropIntent.putExtra("scaleUpIfNeeded",true);
+            CropIntent.putExtra("return-data",true);
+
+            startActivityForResult(CropIntent,1);
+        }
+        catch (ActivityNotFoundException ex)
+        {
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case RequestPermissionCode:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this,"Permission Canceled",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
